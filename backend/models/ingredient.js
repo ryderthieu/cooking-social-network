d441fd06312567f4601eb2c3fe4a8cacc4d8d21e
ryderthieu <ryderthieu@gmai.com>
2025-03-22 23:41:27 +0700

    update backend

--- backend/models/ingredient.js
+++ backend/models/ingredient.js
@@ -2,10 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ingredientSchema = new Schema({
-    name: {type: String, required : true},
-    unit: {type: String},
+    name: { type: String, required: true, unique: true },
+    unit: { type: String },  
+    nutrition: {
+        calories: { type: Number, default: 0 },  // Lượng calo (kcal)
+        protein: { type: Number, default: 0 },   // Chất đạm (g)
+        fat: { type: Number, default: 0 },       // Chất béo (g)
+        carbs: { type: Number, default: 0 }      // Carbohydrate (g)
+    },
+    image: {type: String},
+    author: {type: Schema.ObjectId, ref: 'User'}
 }, {
     timestamps: true
 });
 
-module.exports = mongoose.model('Ingredient', ingredientSchema)
+module.exports = mongoose.model('Ingredient', ingredientSchema);
--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -10,17 +10,24 @@ const recipeSchema = new Schema({
             quantity: {type: Number}
         }
     ],
-    steps: [
-        {
-            step: {type: String, required: true}
-        }
-    ],
-    image: [
-        {
-            uri: {type: String}
-        }
-    ]
-}, {
+    steps: [{
+       step: {type: String, required: true},
+       image: {type: String}
+    }],
+    image: [{type: String}],
+    categories: {
+        mealType: [{ type: String, enum: ["Bữa sáng", "Bữa trưa", "Bữa tối", "Bữa xế", "Món tráng miệng"] }],
+        cuisine: [{ type: String, enum: ["Việt Nam", "Nhật Bản", "Hàn Quốc", "Trung Quốc", "Thái Lan", "Ấn Độ", "Âu", "Mỹ", "Mexico"] }],
+        occasions: [{ type: String, enum: ["Tiệc tùng", "Sinh nhật", "Ngày lễ Tết", "Ăn chay", "Món ăn cho ngày lạnh/nóng"] }],
+        dietaryPreferences: [{ type: String, enum: ["Ăn chay", "Thuần chay", "Keto/Low-carb", "Thực phẩm chức năng", "Không gluten", "Ăn kiêng giảm cân"] }],
+        mainIngredients: [{ type: String, enum: ["Thịt gà", "Thịt bò", "Thịt heo", "Hải sản", "Trứng", "Rau củ", "Đậu phụ"] }],
+        cookingMethod: [{ type: String, enum: ["Chiên", "Nướng", "Hấp", "Xào", "Luộc", "Hầm", "Nấu súp"] }],
+        timeBased: [{ type: String, enum: ["Dưới 15 phút", "15-30 phút", "Trên 1 tiếng"] }],
+        difficultyLevel: { type: String, enum: ["Dễ", "Trung bình", "Khó"] }
+    },
+    utensils: [{type: String}],
+    time: {type: Number}
+}, {      
     timestamps: true
 });
 
